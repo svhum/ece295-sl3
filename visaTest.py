078384507d8633ed7c6f95d28c76a6b652053b12
Sean Victor Hum <sean.hum@utoronto.ca>
2022-01-31 08:54:15 -0500

    Revisions per Stewart 01/30

--- visaTest.py
+++
@@ -1,6 +0,0 @@
-# This program makes sure that libraries are all installed correctly
-# by Ian Bennett
-import pyvisa as visa
-rm = visa.ResourceManager(r'C:\WINDOWS\system32\visa64.dll')
-print(rm)
-print(rm.list_resources('TCPIP0::?*'))
